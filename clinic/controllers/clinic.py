21a7223cafc1d6330bcd0258d8640c5ae05f9c3b
Ri-dha <tareqridha@gmail.com>
2022-10-15 16:26:41 +0300

    0.75(get all clinics and delete)

--- clinic/controllers/clinic.py
+++ clinic/controllers/clinic.py
@@ -9,6 +9,12 @@ from clinic.schemas import ClinicIn, ClinicOut
 clinic_controller = Router(tags=['Clinic'])
 
 
+@clinic_controller.get('clinic/show/all', auth=AuthBearer(), response={200: ClinicOut, 403: MessageOut})
+def get_all_clinic(request, per_page: int = 12, page: int = 1):
+    clinics = Clinic.objects.all()
+    return response(200, clinics, paginated=True, per_page=per_page, page=page)
+
+
 @clinic_controller.post('clinic', auth=AuthBearer(), response={200: ClinicOut, 403: MessageOut})
 def clinic_create(request, payload: ClinicIn):
     if request.auth.is_staff:
@@ -25,11 +31,11 @@ def clinic_create(request, payload: ClinicIn):
             return 200, clinic
 
 
-
-@clinic_controller.get('clinic/show/{pk}', auth=AuthBearer(), response={200: ClinicOut , 403: MessageOut})
+@clinic_controller.get('clinic/show/{pk}', auth=AuthBearer(), response={200: ClinicOut, 403: MessageOut})
 def get_clinic(request, pk: str):
     clinic = Clinic.objects.get(id=pk)
     return 200, clinic
+
 
 @clinic_controller.put('clinic/update/{pk}', auth=AuthBearer(), response={200: ClinicOut, 403: MessageOut})
 def update_clinic(request, pk: str, payload: ClinicIn):
@@ -40,5 +46,16 @@ def update_clinic(request, pk: str, payload: ClinicIn):
             clinic.address = payload.address
             clinic.save()
             return 200, clinic
+        except Clinic.DoesNotExist:
+            return 403, {'message': 'Clinic does not exist'}
+
+
+@clinic_controller.delete('clinic/delete/{pk}', auth=AuthBearer(), response={200: ClinicOut, 403: MessageOut})
+def delete_clinic(request, pk: str):
+    if request.auth.is_staff:
+        try:
+            clinic = Clinic.objects.get(id=pk)
+            clinic.delete()
+            return 200, {'message': 'Clinic deleted successfully'}
         except Clinic.DoesNotExist:
             return 403, {'message': 'Clinic does not exist'}
